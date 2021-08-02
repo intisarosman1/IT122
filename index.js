4b18da2b5a5f774930baa9a20c8186fc856baae4
Intisar Osman <intisarosman3@gmail.com>
2021-08-02 16:20:24 -0700

    Homework 5

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 import express from 'express';
 import handlebars from "express-handlebars";
 import { Book } from './book.js';
+import cors from 'cors';
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use(express.static('./public')); // allows direct navigation to static files
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
+app.use('/api', cors());
 
 app.engine('hbs', handlebars({defaultLayout: false}));
 app.set("view engine", "hbs");
@@ -43,7 +45,63 @@ app.post('/detail', (req,res, next) => {
         })
         .catch(err => next(err));
 });
-// define 404 handler
+
+
+
+
+
+app.get('/api/books', (req,res) => {
+    Book.find({}).lean()
+        .then((books) => {
+            res.json(books)
+        })
+        .catch(err => next(err));
+});
+
+   app.get('/api/book/:title', (req, res, next) => {
+    let title = req.params.title;
+    console.log(title);
+    Book.findOne({title: title}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json( result );    
+    });
+});
+
+app.get('/api/book/delete/:id', (req,res, next) => {
+    Book.deleteOne({"_id":req.params.id }, (err, result) => {
+        if (err) return next(err);
+        console.log(result)
+        res.json({"deleted": result});
+    });
+});
+
+app.post('/api/book/add/', (req,res, next) => {
+    console.log(req.body)
+    if (!req.body._id) {
+
+        let book = new Book({title:req.body.title,author:req.body.author,genre:req.body.genre,pages:req.body.pages,price:req.body.price});
+        book.save((err,newBook) => {
+            if (err) return next(err);
+            console.log(newBook)
+            res.json({updated: 0, _id: newBook._id});
+        });
+    } else {
+        Book.updateOne({ _id: req.body._id}, {title:req.body.title, author: req.body.author, genre: req.body.genre, pages: req.body.pages, price: req.body.price }, (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
+});
+
+app.get('/api/book/add/:title/:author/:genre/:pages/:price', (req,res, next) => {
+    let title = req.params.title;
+    Book.update({ title: title}, {title:title, author: req.params.author, genre: req.params.genre, pages: req.params.pages, price: req.params.price }, {upsert: true }, (err, result) => {
+        if (err) return next(err);
+        res.json({updated: result.nModified});
+    });
+});
+
+
 app.use((req,res) => {
     res.type('text/plain'); 
     res.status(404);
